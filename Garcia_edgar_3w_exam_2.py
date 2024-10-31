b54edff9e272172c945a8bca53c7a2beb0ca0e81
Galletas <a.23308051281182@cbtis128.edu.mx>
2024-10-31 16:00:19 -0600

    Add files via upload

---
+++ Garcia_edgar_3w_exam_2.py
@@ -0,0 +1,18 @@
+print()
+print("Edgar Gael Garcia Camacho 1182:Examen.")
+print()
+
+# Programa #2.
+Asignaturas={
+    "Matematicas" : 9 ,
+    "Español" : 5 ,
+    "Programacion" : 9
+}
+print(Asignaturas)
+
+Asignaturas = (9 + 5 + 9)
+Total_de_creditos = 0
+
+suma = (Asignaturas + Total_de_creditos)
+print()
+print("Los creditos totales son :", suma)
---
+++ Garcia_edgar_3w_exam_3.py
@@ -0,0 +1,19 @@
+print()
+print("Edgar gael garcia Camacho 1182: Examen.")
+print()
+Lista=["Matamaticas","Español","Programación"]
+print(Lista)
+print()
+# Programa #3
+Matematicas = float(input("Ingresa tu calificacion de matematicas :"))
+print()
+Español = float(input("Ingresa tu calificación de Español :"))
+print()
+programación = float(input("Ingresa tu calificación de Programación :"))
+print()
+
+print("Has sacado esta nota en matematicas :", Matematicas)
+print()
+print("Has sacado esta nota en español :",Español)
+print()
+print("Has sacado esta nota en programación :",programación)
